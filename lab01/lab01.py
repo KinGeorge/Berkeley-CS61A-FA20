4caa1292acf903c16e4be1f35048ae7c3e2a235e
KinGeorge <47409321+KinGeorge@users.noreply.github.com>
2022-08-04 11:54:51 +0800

    Add files via upload

---
+++ lab01/lab01.py
@@ -0,0 +1,63 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    if not k:
+        return 1
+    result = n
+    for i in range(k-1):
+        result = result * (n-i-1)
+
+    return result
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    digits = []
+    string = str(y)
+    for i in range(len(string)):
+        digits.append(int(string[i]))
+
+    return sum(digits)
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    string = str(n)
+    if string.count('88'):
+        return True
+    return False
+
